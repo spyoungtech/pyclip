e8fe6b4e50c91f804d4ae4b7d208c638477f05d6
Spencer Phillip Young <spencer.young@spyoung.com>
2021-01-29 13:37:00 -0800

    v0.4.0 :package:
    adds linux support!

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='pyperclip3',
-    version='0.3.0',
+    version='0.4.0',
     license='Apache',
     url='https://github.com/spyoungtech/pyperclip3',
     description='Cross-platform clipboard utilities supporting both binary and text data.',
