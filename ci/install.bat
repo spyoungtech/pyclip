688e44ca328091059934d12d9d3b6905d98925cf
Spencer Phillip Young <spencer.young@spyoung.com>
2021-02-02 05:51:38 -0800

    install older version of codecov

--- ci/install.bat
+++ ci/install.bat
@@ -1,6 +1,6 @@
 py -3.8 -m venv venv
 call venv\Scripts\activate.bat
 python -m pip install --upgrade pip
-python -m pip install --upgrade wheel pytest coverage codecov
+python -m pip install --upgrade wheel pytest coverage "codecov<5"
 python -m pip install --upgrade .
 call deactivate
