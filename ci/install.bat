bd2bc55835f2a09ea4aa65e0b984b654ae701b9a
Spencer Phillip Young <spencer.young@spyoung.com>
2022-03-18 11:28:04 -0700

    remove unused files

--- ci/install.bat
+++
@@ -1,6 +0,0 @@
-py -3.8 -m venv venv
-call venv\Scripts\activate.bat
-python -m pip install --upgrade pip
-python -m pip install --upgrade wheel pytest coverage codecov
-python -m pip install --upgrade .
-call deactivate
