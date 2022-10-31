f11fce5d7a2541ac6e42c24bc61e42e99bacac25
Spencer Phillip Young <spencer.young@spyoung.com>
2022-10-31 14:12:52 -0700

    v0.7.0 :package:

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('docs/README.md', encoding='utf-8') as f:
 
 setup(
     name='pyclip',
-    version='0.6.0',
+    version='0.7.0',
     license='Apache',
     url='https://github.com/spyoungtech/pyclip',
     description='Cross-platform clipboard utilities supporting both binary and text data.',
